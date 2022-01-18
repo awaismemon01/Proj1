1297e24c86a850b123a5052d08af0fd0f8998066
awaismemon01 <awaismemon2323@gmail.com>
2022-01-18 14:41:48 +0530

    commit

--- src/First.java
+++ src/First.java
@@ -1,2 +1,9 @@
 public class First {
+    public static void main(String args[]){
+     int num =100;
+     System.out.println("Number : "+ num);
+     num *= 2;
+     System.out.println("2 times number is :"+ num);
+    }
 }
+
