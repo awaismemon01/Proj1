6bc88307bae43dfd4de3bd56adc3434ec01dd1be
Mohammed Awais Memon <awaismemon2323@gmail.com>
2022-02-04 18:44:26 +0530

    feat: add a program to multiply float numbers

---
+++ src/HandsOnPractice/MultiplyFloatNumbers.java
@@ -0,0 +1,13 @@
+package HandsOnPractice;
+
+//Java Program to Multiply Two Floating Numbers
+
+public class MultiplyFloatNumbers {
+    public static void main(String[] args) {
+
+        float f1 = (float) Math.random()*100;
+        float f2 = (float) Math.random()*100;
+        System.out.println("The multiplication of "+f1+" and "+f2+" is "+(f1*f2));
+
+    }
+}
